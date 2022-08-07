c17ede3d135f294021f99ed2169d083a51e515a8
Junev <lijun58518@126.com>
2022-08-07 16:33:25 +0800

    close banner

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//		SpringApplication.run(DemoApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
+		builder.bannerMode(Banner.Mode.OFF).run(args);
 	}
 
 }
