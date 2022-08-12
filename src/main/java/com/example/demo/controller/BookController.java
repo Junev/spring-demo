75ef8b210967ed94e0568449300616e3a1a97455
Junev <lijun58518@126.com>
2022-08-12 14:47:08 +0800

    json converter

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,13 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class BookController {
     @Autowired
     Book book;
 
+    /**
+     * MappingJacksonToHttpMessageConverter
+     *
+     */
     @GetMapping("/book")
-    public String book() {
-        return book.toString();
+    public Book book() {
+        Book newBook = new Book();
+        newBook.setName("三国演义");
+        newBook.setAuthor("罗贯中");
+        newBook.setPrice(129.99);
+        newBook.setPublicationDate(new Date());
+        return newBook;
     }
 }
--- src/main/java/com/example/demo/entities/Book.java
+++ src/main/java/com/example/demo/entities/Book.java
@@ -1,37 +1,54 @@
 package com.example.demo.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 @ConfigurationProperties(prefix = "mybook")
 public class Book {
     private String name;
     private String author;
+
+    @JsonIgnore
     private double price;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date publicationDate;
 
     public String getName() {
         return this.name;
-    }
-
-    public String getAuthor() {
-        return this.author;
-    }
-
-    public double getPrice() {
-        return this.price;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public String getAuthor() {
+        return this.author;
+    }
+
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    public double getPrice() {
+        return this.price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
     }
 
     @Override
