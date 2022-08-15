eb1660626bd49fb094cf999dd8f35f4199b8fa84
Junev <lijun58518@126.com>
2022-08-15 15:32:54 +0800

    file upload exception handler

---
+++ src/main/java/com/example/demo/exceptionHandler/CustomExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.example.demo.exceptionHandler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public void uploadException(MaxUploadSizeExceededException e, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html;chart-set=utf-8");
+        PrintWriter pw = res.getWriter();
+        pw.write("上传文件超出大小限制。");
+        pw.flush();
+        pw.close();
+    }
+}
