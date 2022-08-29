0dba8d43f4b6f47d6a37b7aa5dedf5025ece4b52
Junev <lijun58518@126.com>
2022-08-29 17:52:27 +0800

    Servlet, Filter, Listener

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.example.demo;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/WebMvcConfigurer/MyWebMvcConfig.java
+++ src/main/java/com/example/demo/WebMvcConfigurer/MyWebMvcConfig.java
@@ -1,7 +1,9 @@
 package com.example.demo.WebMvcConfigurer;
 
+import com.example.demo.interceptor.MyIntercepter1;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +15,12 @@ public class MyWebMvcConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .maxAge(1800)
                 .allowedOrigins("http://localhost:8080");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyIntercepter1())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/hello");
     }
 }
