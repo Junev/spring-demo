db0a1d22b872300fe17dbda01d4ecdcc7e995d29
Junev <lijun58518@126.com>
2022-10-14 09:24:16 +0800

    add swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -15,10 +21,23 @@ import org.springframework.web.client.RestTemplate;
 @MapperScan("com.example.repository.mapper")
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-//        SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
-//        builder.bannerMode(Banner.Mode.OFF).run(args);
+    public static void main(String[] args) throws UnknownHostException {
+//        SpringApplication.run(DemoApplication.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoApplication.class);
+        ConfigurableApplicationContext application = builder.bannerMode(Banner.Mode.OFF).run(args);
+
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        String path = contextPath != null ? contextPath : "";
+        System.out.println("\n----------------------------------------------------------\n\t" +
+                "Application demo is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui/index.html\n\t" +
+//                "Doc文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
     @Autowired
