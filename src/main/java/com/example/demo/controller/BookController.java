4ee879eebb7c58d4c34f319dbbdc9211427e0149
Junev <lijun58518@126.com>
2022-10-21 22:43:56 +0800

    lettuce redis config

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,19 +1,30 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Book;
+import io.lettuce.core.LettuceFutures;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {
     @Autowired
     Book book;
+
+    @Autowired
+    RedisClient redisClient;
 
     @Autowired
     RedisTemplate redisTemplate;
@@ -25,7 +36,19 @@ public class BookController {
      * MappingJacksonToHttpMessageConverter
      */
     @GetMapping("")
-    public Book book() {
+    public Book book() throws ExecutionException, InterruptedException {
+        StatefulRedisConnection<String, String> connect = redisClient.connect();
+        RedisAsyncCommands<String, String> asyncCommands = connect.async();
+        RedisFuture<String> get1 = asyncCommands.get("name");
+        RedisFuture<String> set1 = asyncCommands.set("name", "foo bar");
+
+        ArrayList<RedisFuture<String>> redisFutures = new ArrayList<>();
+        redisFutures.add(get1);
+        redisFutures.add(set1);
+        LettuceFutures.awaitAll(1, TimeUnit.MINUTES,
+                redisFutures.toArray(new RedisFuture[redisFutures.size()]));
+
+
         Book newBook = new Book();
         newBook.setName("三国演义");
         newBook.setAuthor("罗贯中");
