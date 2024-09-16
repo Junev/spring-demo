81eb06c8428750af2cb91f50b737e704e653901e
lijun <lijun58518@126.com>
2024-09-16 10:00:26 +0800

    feat: 添加启停服务脚本。

--- modules/mytask/src/main/resources/install.bat
+++ modules/mytask/src/main/resources/install.bat
@@ -15,6 +15,7 @@ set MAIN_CLASS=org.springframework.boot.loader.JarLauncher
  
 rem 设置prunsrv路径
 set SRV=%BASEDIR%\prunsrv.exe
+set MGR=%BASEDIR%\prunmgr.exe
  
 rem 设置日志路径及日志文件前缀
 set LOGPATH=%BASEDIR%
@@ -40,7 +41,23 @@ goto foundJvm
 :foundJvm
 echo 正在安装服务...
 rem 安装
-"%SRV%" //IS//%SERVICE_EN_NAME% --DisplayName="%SERVICE_CH_NAME%" "--Classpath=%CLASSPATH%" "--Install=%SRV%" "--JavaHome=%JAVA_HOME%" "--Jvm=%JVM%" --JvmMs=256 --JvmMx=1024 --Startup=auto --JvmOptions=-Djcifs.smb.client.dfs.disabled=false ++JvmOptions=-Djcifs.resolveOrder=DNS --StartMode=jvm --StartClass=%MAIN_CLASS% --StartMethod=main --StopMode=jvm --StopClass=%MAIN_CLASS% --StopMethod=main --StopParams=  --LogPath=%LOGPATH% --StdOutput=auto --StdError=auto
+%SRV% //IS//%SERVICE_EN_NAME% --DisplayName="%SERVICE_CH_NAME%" ^
+        "--Classpath=%CLASSPATH%" "--Install=%SRV%" ^
+        "--JavaHome=%JAVA_HOME%" "--Jvm=%JVM%" --JvmMs=256 --JvmMx=1024 ^
+        --Startup=auto ^
+        --JvmOptions=-Djcifs.smb.client.dfs.disabled=false ^
+        ++JvmOptions=-Djcifs.resolveOrder=DNS ^
+        --StartMode=jvm ^
+        --StartClass=%MAIN_CLASS% ^
+        --StartMethod=main ^
+        --StopMode=jvm ^
+        --StopClass=%MAIN_CLASS% ^
+        --StopMethod=main ^
+        --StopParams= ^
+        --StopTimeout=3 ^
+        --LogPath=%LOGPATH% ^
+        --StdOutput=auto ^
+        --StdError=auto
 echo 安装服务完成。
 :end
 pause
